8c0530ebe80adfe97abc5d361a7f55449bc92f21
Mahir Durna <durna.m@outlook.de>
2026-03-08 19:05:32 +0100

    feat: new data protection and imprint

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function init (){
+  customElements.define("include-html", IncludeHTML);
+}
+
+class IncludeHTML extends HTMLElement {
+  async connectedCallback() {
+    const file = this.getAttribute("src");
+    try {
+      const response = await fetch(file);
+      if (!response.ok)
+        throw new Error(
+          `Failed to fetch ${file}: ${response.statusText}`
+        );
+      this.innerHTML = await response.text();
+    } catch (error) {
+      this.innerHTML = "Page not found";
+      console.error(error);
+    }
+  }
+}
+
+
+
 // ── LANGUAGE TOGGLE ──
 document.querySelectorAll('.lang-toggle span').forEach((btn) => {
   btn.addEventListener('click', () => {
